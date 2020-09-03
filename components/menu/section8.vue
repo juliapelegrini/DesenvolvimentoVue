04d04fe530a4dd00e6a2693b790fa53b8eab42c8
julia <julia.pelegrini26@gmail.com>
2020-09-03 16:26:50 -0300

    contrate também automatizado

--- components/menu/section8.vue
+++ components/menu/section8.vue
@@ -1,75 +1,26 @@
 <template>
   <div class="section8">
     <div class="tit_s8">Contrate também (opcional)</div>
-    <div class="text1_s8">
-      Aproveite outros serviços para obter mais desempenho em sua loja
-      virtual. Estes são serviços opcionais e também possuem 30 <br />
-      dias grátis com exceção da integração
-      com sistemas de terceiros.
-    </div>
+    <div class="text1_s8 col-9" v-html="contrate.text1"></div>
     <div class="container">
       <div class="row">
-        <div class="col-xl-1 col-lg-1 col-md-1 col-sm-12 col-12"></div>
-        <div class="col-xl-3 col-lg-3 col-md-3 col-sm-12 col-12">
-          <img class="img_s8" src="../../assets/dominio.svg" alt="dominio" />
-          <p class="text2_s8">Registro de domínio</p>
-          <p class="text3_s8">
-            Seu domínio www.sualoja.com.br <br />
-            pago ao registro.br
-          </p>
-          <p class="text4_s8">R$40,00/ano</p>
-        </div>
-        <div class="col-xl-1 col-lg-1 col-md-1 col-sm-12 col-12"></div>
-        <div class="col-xl-3 col-lg-3 col-md-3 col-sm-12 col-12">
-          <img class="img_s8" src="../../assets/email.svg" alt="e-mail" />
-          <p class="text2_s8">E-mail personalizado</p>
-          <p class="text3_s8">
-            E-mail Zimbra espaço de 1GB preço por conta de e-mail
-          </p>
-          <p class="text4_s8">R$3,90/mês</p>
-        </div>
-        <div class="col-xl-1 col-lg-1 col-md-1 col-sm-12 col-12"></div>
-        <div class="col-xl-3 col-lg-3 col-md-3 col-sm-12 col-12">
-          <img class="img_s8" src="../../assets/api.svg" alt="API" />
-          <p class="text2_s8">Integrações de terceiros</p>
-          <p class="text3_s8">
-            Implantação R$400,00/integração
-            mensalidade por integração
-          </p>
-          <p class="text4_s8">R$49,90/mês</p>
-        </div>
-      </div>
-
-      <div class="row">
-        <div class="col-xl-1 col-lg-1 col-md-1 col-sm-12 col-12"></div>
-        <div class="col-xl-3 col-lg-3 col-md-3 col-sm-12 col-12">
-          <img class="img_s8" src="../../assets/carrinho.svg" alt="carrinho" />
-          <p class="text2_s8">Recuperação de carrinho</p>
-          <p class="text3_s8">
-            Aumente a conversão de suas vendas!preço por lote de
-            1.000 disparos
-          </p>
-          <p class="text4_s8">R$9,90/mês</p>
-        </div>
-        <div class="col-xl-1 col-lg-1 col-md-1 col-sm-12 col-12"></div>
-        <div class="col-xl-3 col-lg-3 col-md-3 col-sm-12 col-12">
-          <img class="img_s8" src="../../assets/marketing.svg" alt="marketing" />
-          <p class="text2_s8">E-mail marketing</p>
-          <p class="text3_s8">
-            Dispare suas campanhas por e-mail! preço por lote de 1.000
-            disparos
-          </p>
-          <p class="text4_s8">R$9,90/mês</p>
-        </div>
-        <div class="col-xl-1 col-lg-1 col-md-1 col-sm-12 col-12"></div>
-        <div class="col-xl-3 col-lg-3 col-md-3 col-sm-12 col-12">
-          <img class="img_s8" src="../../assets/venda.svg" alt="Venda" />
-          <p class="text2_s8">Ponto de Venda - PDV</p>
-          <p class="text3_s8">
-            Utilize para venda na sua loja física
-            configuração de cupom não fiscal
-          </p>
-          <p class="text4_s8">R$150,00 único</p>
+        <div
+          class="col-xl-4 col-lg-4 col-md-4 col-sm-12 col-12"
+          v-for="data in contrate.category1.list"
+          :key="data.id"
+        >
+          <img
+            :src="data.image1"
+            :title="data.name"
+            :alt="data.name"
+            class="img_s8"
+          />
+          <div class="text2_s8">{{ data.name1 }}</div>
+          <div
+            class="text3_s8 col-xl-11 col-lg-11 col-md-11 col-sm-12 col-12"
+            v-html="data.text1"
+          ></div>
+          <div class="text4_s8" v-html="data.text2"></div>
         </div>
       </div>
     </div>
@@ -78,12 +29,27 @@
 </template>
 
 <script>
-export default {};
+export default {
+  data() {
+    return {
+      contrate: []
+    };
+  },
+  async fetch() {
+    this.contrate = await this.$axios
+      .$get(
+        "https://wdshop-j.dev.wdhouse.com.br/wdframe/api/v1/cms?page_id=14&categories_content=true"
+      )
+      .then(res => {
+        return res.data;
+      });
+  }
+};
 </script>
 
 <style>
-.section8{
-    width: 100%;
+.section8 {
+  width: 100%;
 }
 
 .tit_s8 {
@@ -102,6 +68,7 @@ export default {};
   opacity: 60%;
   margin-top: 20px;
   margin-bottom: 50px;
+  margin-left: 200px;
 }
 
 .text2_s8 {
